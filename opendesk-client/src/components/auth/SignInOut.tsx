4792233204dd628079dbf5226152157e2a6330f4
Eric M <itsjusteza@gmail.com>
2021-06-10 22:29:52 +1000

    Improve sign-out button styling.

--- opendesk-client/src/components/auth/SignInOut.tsx
+++ opendesk-client/src/components/auth/SignInOut.tsx
@@ -3,9 +3,19 @@ import Authenticated from "components/auth/Authenticated";
 
 import { ReactComponent as MsSigningDarkButton } from "resources/ms-signin-dark.svg";
 import { useAuth } from "hooks/useAuth";
+import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		signOutButton: {
+			marginLeft: "1em",
+		},
+	})
+);
 
 function SignInOut() {
 	const { user, signIn, signOut } = useAuth();
+	const classes = useStyles();
 
 	return (
 		<div className="sign-in-out">
@@ -20,12 +30,13 @@ function SignInOut() {
 			<Authenticated>
 				<div className="sign-in-out__out-container">
 					<p>{user?.name}</p>
-					<button
-						className="sign-in-out__out-btn"
+					<Button
+						className={classes.signOutButton}
+						variant="contained"
 						onClick={() => signOut(window.location.origin)}
 					>
 						Sign Out
-					</button>
+					</Button>
 				</div>
 			</Authenticated>
 		</div>
