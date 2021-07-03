35652798f1a1ac7bd0dc6201d7ab4ef1f518be3c
Eric M <itsjusteza@gmail.com>
2021-07-03 15:29:47 +1000

    Add filter for offices list.

--- opendesk-client/src/pages/ManageOffices.tsx
+++ opendesk-client/src/pages/ManageOffices.tsx
@@ -2,22 +2,43 @@ import {
 	Box,
 	Button,
 	Card,
-	CardContent,
+	createStyles,
 	Grid,
 	List,
 	ListItem,
 	ListItemText,
+	makeStyles,
+	TextField,
+	Theme,
+	Typography,
 } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 import OfficeDetailsEditor from "components/OfficeDetailsEditor";
 import { StatusData } from "components/StatusAlert";
 import { useOffices } from "hooks/useOffices";
 import { usePageTitle } from "hooks/usePageTitle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Office, ValidationError } from "types";
 import apiRequest from "utils/requestUtils";
 
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		officeListContainer: {
+			padding: "1rem",
+		},
+		officeListHeader: {
+			display: "flex",
+			marginBottom: theme.spacing(2),
+		},
+		officeListFilter: {
+			flexGrow: 1,
+			marginRight: "1rem",
+		},
+	})
+);
+
 export default function ManageOffices() {
+	const classes = useStyles();
 	usePageTitle("Manage offices");
 
 	const {
@@ -27,6 +48,27 @@ export default function ManageOffices() {
 
 	const [selectedOffice, setSelectedOffice] = useState<Office | null>();
 	const [statusData, setStatusData] = useState<StatusData>({});
+
+	const [filterValue, setFilterValue] = useState<string>();
+	const [displayedOffices, setDisplayedOffices] = useState(offices);
+
+	useEffect(() => {
+		if (!filterValue) {
+			setDisplayedOffices(offices);
+			return;
+		}
+
+		const lowerFilter = filterValue.toLocaleLowerCase();
+
+		setDisplayedOffices(
+			offices?.filter(
+				(o) =>
+					o.name.toLocaleLowerCase().indexOf(lowerFilter) > -1 ||
+					o.location.toLocaleLowerCase().indexOf(lowerFilter) > -1 ||
+					o.subLocation.toLocaleLowerCase().indexOf(lowerFilter) > -1
+			)
+		);
+	}, [filterValue, offices]);
 
 	function onOfficeSelected(office: Office) {
 		setSelectedOffice(office);
@@ -94,34 +136,49 @@ export default function ManageOffices() {
 		<>
 			<Grid container spacing={2}>
 				<Grid item md={3} xs={12}>
-					<Card variant="outlined">
-						<CardContent>
-							<List>
-								<Box textAlign="right" marginBottom={2}>
-									<Button
-										variant="contained"
-										color="primary"
-										startIcon={<Add />}
-										onClick={() => onNewOfficeClicked()}
-									>
-										New Office
-									</Button>
-								</Box>
-								{offices?.map((o) => (
-									<ListItem
-										key={o.id}
-										button
-										selected={selectedOffice?.id === o.id}
-										onClick={(e) => onOfficeSelected(o)}
-									>
-										<ListItemText
-											primary={o.name}
-											secondary={`${o.location} / ${o.subLocation}`}
-										/>
-									</ListItem>
-								))}
-							</List>
-						</CardContent>
+					<Card variant="outlined" className={classes.officeListContainer}>
+						<List>
+							<Box className={classes.officeListHeader}>
+								<TextField
+									className={classes.officeListFilter}
+									variant="outlined"
+									size="small"
+									placeholder="Filter..."
+									inputProps={{
+										onInput: (e) => setFilterValue(e.currentTarget.value),
+									}}
+								/>
+								<Button
+									variant="contained"
+									color="primary"
+									startIcon={<Add />}
+									onClick={() => onNewOfficeClicked()}
+								>
+									New Office
+								</Button>
+							</Box>
+							{displayedOffices?.map((o) => (
+								<ListItem
+									key={o.id}
+									button
+									selected={selectedOffice?.id === o.id}
+									onClick={(e) => onOfficeSelected(o)}
+								>
+									<ListItemText
+										primary={o.name}
+										secondary={`${o.location} / ${o.subLocation}`}
+									/>
+								</ListItem>
+							))}
+							{offices && offices.length === 0 && (
+								<Typography>There are no offices.</Typography>
+							)}
+							{offices &&
+								offices.length !== 0 &&
+								displayedOffices?.length === 0 && (
+									<Typography>No offices match the filter.</Typography>
+								)}
+						</List>
 					</Card>
 				</Grid>
 				{selectedOffice !== undefined && (
