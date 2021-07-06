870af8ba707e4055a9aefb056b2abf13f848e2d6
Eric M <itsjusteza@gmail.com>
2021-07-06 10:14:08 +1000

    Update OfficeSelector - added labels for each input and added optional "compact" mode, which only has 1 dropdown allowing to select all offices.
    The dialog groups the offices by the combination of Location and SubLocation

--- opendesk-client/src/components/OfficeSelector.tsx
+++ opendesk-client/src/components/OfficeSelector.tsx
@@ -1,14 +1,49 @@
-import { useEffect, useState, useMemo } from "react";
-import { Breadcrumbs, Select, MenuItem } from "@material-ui/core";
-import { NavigateNext } from "@material-ui/icons";
+import { useEffect, useMemo } from "react";
+import {
+	Breadcrumbs,
+	Select,
+	MenuItem,
+	List,
+	ListItem,
+	Collapse,
+	ListItemIcon,
+	ListItemText,
+	ListSubheader,
+	FormControl,
+	InputLabel,
+	Box,
+	makeStyles,
+	Theme,
+	createStyles,
+} from "@material-ui/core";
+import { NavigateNext, StarBorder } from "@material-ui/icons";
 import { useOffices } from "hooks/useOffices";
 import useOfficeDeskRouteParams from "hooks/useOfficeDeskRouteParams";
 import useOfficeLocationFilter from "hooks/useOfficeLocationFilter";
+import { Office } from "types";
 
-function OfficeSelector() {
+type OfficeMapping = {
+	[key: string]: Office[];
+};
+
+type OfficeSelectorProps = {
+	compact?: boolean;
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		comapctFormControl: {
+			width: "100%",
+		},
+	})
+);
+
+function OfficeSelector({ compact = false }: OfficeSelectorProps) {
 	// =============================================================
 	// Hooks and Variables
 	// =============================================================
+
+	const classes = useStyles();
 
 	const { officeIdParam, setOfficeParam } = useOfficeDeskRouteParams();
 	const {
@@ -23,6 +58,28 @@ function OfficeSelector() {
 		selectedLocationState: [selectedLocation, setSelectedLocation],
 		selectedSublocationState: [selectedSubLocation, setSelectedSublocation],
 	} = useOfficeLocationFilter();
+
+	const officeMapping = useMemo<OfficeMapping>(
+		() =>
+			offices
+				// Sort by location and then sublocation
+				?.sort(
+					(o1, o2) =>
+						o1.location.localeCompare(o2.location) ||
+						o1.subLocation.localeCompare(o2.subLocation)
+				)
+				// Create a dictionary where the keys are "Location / Sublocation" and the values are the offices within those combinations
+				.reduce((map, office) => {
+					const key = `${office.location} / ${office.subLocation}`;
+					const currValue = [...(map[key] || [])];
+
+					return {
+						...map,
+						[key]: [...currValue, office],
+					};
+				}, {} as OfficeMapping) ?? ({} as OfficeMapping),
+		[offices]
+	);
 
 	// =============================================================
 	// Effects
@@ -71,63 +128,92 @@ function OfficeSelector() {
 
 	return (
 		<>
-			<Breadcrumbs separator={<NavigateNext />}>
-				{/* Location */}
-				<Select
-					value={selectedLocation ?? ""}
-					onChange={(e) => {
-						handleLocationChange(e.target.value as string);
-					}}
-					displayEmpty
-				>
-					<MenuItem value="">
-						<em>No Selection</em>
-					</MenuItem>
-					{locations?.map((ol) => (
-						<MenuItem key={ol} value={ol}>
-							{ol}
+			{!compact && (
+				<Breadcrumbs separator={<NavigateNext />}>
+					{/* Location */}
+					<FormControl>
+						<InputLabel shrink>Location</InputLabel>
+						<Select
+							value={selectedLocation ?? ""}
+							onChange={(e) => {
+								handleLocationChange(e.target.value as string);
+							}}
+							displayEmpty
+						>
+							<MenuItem value="">
+								<em>No Selection</em>
+							</MenuItem>
+							{locations?.map((ol) => (
+								<MenuItem key={ol} value={ol}>
+									{ol}
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+
+					{/* Sub Location */}
+					<FormControl>
+						<InputLabel shrink>Sub Location</InputLabel>
+						<Select
+							value={selectedSubLocation}
+							onChange={(e) => {
+								handleSubLocationChange(e.target.value as string);
+							}}
+							disabled={selectedLocation === ""}
+							displayEmpty
+						>
+							<MenuItem value="">
+								<em>No Selection</em>
+							</MenuItem>
+							{subLocations?.map((osl) => (
+								<MenuItem key={osl} value={osl}>
+									{osl}
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+
+					{/* Office */}
+					<FormControl>
+						<InputLabel shrink>Office</InputLabel>
+						<Select
+							value={selectedOffice?.id ?? ""}
+							onChange={(e) => handleOfficeChange(e.target.value as string)}
+							disabled={selectedSubLocation === ""}
+							displayEmpty
+						>
+							<MenuItem value="">
+								<em>No Selection</em>
+							</MenuItem>
+							{availableOffices?.map((o) => (
+								<MenuItem key={o.id} value={o.id}>
+									{o.name}
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Breadcrumbs>
+			)}
+			{compact && (
+				<FormControl className={classes.comapctFormControl}>
+					<InputLabel shrink>Select an Office...</InputLabel>
+					<Select
+						value={selectedOffice?.id ?? ""}
+						onChange={(e) => handleOfficeChange(e.target.value as string)}
+						displayEmpty
+					>
+						<MenuItem value="">
+							<em>No Selection</em>
 						</MenuItem>
-					))}
-				</Select>
-
-				{/* Sub Location */}
-
-				<Select
-					value={selectedSubLocation}
-					onChange={(e) => {
-						handleSubLocationChange(e.target.value as string);
-					}}
-					disabled={selectedLocation === ""}
-					displayEmpty
-				>
-					<MenuItem value="">
-						<em>No Selection</em>
-					</MenuItem>
-					{subLocations?.map((osl) => (
-						<MenuItem key={osl} value={osl}>
-							{osl}
-						</MenuItem>
-					))}
-				</Select>
-
-				{/* Office */}
-
-				<Select
-					value={selectedOffice?.id ?? ""}
-					onChange={(e) => handleOfficeChange(e.target.value as string)}
-					disabled={selectedSubLocation === ""}
-					displayEmpty
-				>
-					<MenuItem value="">
-						<em>No Selection</em>
-					</MenuItem>
-					{availableOffices?.map((o) => (
-						<MenuItem key={o.id} value={o.id}>
-							{o.name}
-						</MenuItem>
-					))}
-				</Select>
-			</Breadcrumbs>
+						{Object.keys(officeMapping).map((k) => [
+							<ListSubheader>{k}</ListSubheader>,
+							officeMapping[k]?.map((o) => (
+								<MenuItem value={o.id}>{o.name}</MenuItem>
+							)),
+						])}
+					</Select>
+				</FormControl>
+			)}
 		</>
 	);
 }
