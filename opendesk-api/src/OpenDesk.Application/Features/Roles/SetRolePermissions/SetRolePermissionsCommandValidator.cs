1aaa621387f3cbecef6d70fcee697f35d050e7e5
Eric M <itsjusteza@gmail.com>
2021-10-11 09:02:59 +1000

    Add validators for SetRolePermissions

--- opendesk-api/src/OpenDesk.Application/Features/Roles/SetRolePermissions/SetRolePermissionsCommandValidator.cs
+++ opendesk-api/src/OpenDesk.Application/Features/Roles/SetRolePermissions/SetRolePermissionsCommandValidator.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using OpenDesk.Application.Common;
+using OpenDesk.Application.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,34 @@ using System.Threading.Tasks;
 
 namespace OpenDesk.Application.Features.Roles
 {
-	class SetRolePermissionsCommandValidator
+	class SetRolePermissionsCommandValidator : AbstractValidator<SetRolePermissionsCommand>
 	{
-		// TODO
+		private readonly RoleManager<OpenDeskRole> _roleManager;
+
+		public SetRolePermissionsCommandValidator(RoleManager<OpenDeskRole> roleManager)
+		{
+			_roleManager = roleManager;
+
+			RuleFor(p => p.RoleId)
+				.MustAsync((command, _, _) => ValidateRoleExists(command))
+				.WithMessage("Role does not exist.")
+				.MustAsync((command, _, _) => ValidateNotChangingSuperAdmin(command))
+				.WithMessage("SuperAdmin cannot have permissions changed.")
+				.NotEmpty();
+		}
+
+		private async Task<bool> ValidateRoleExists(SetRolePermissionsCommand command)
+		{
+			var role = await _roleManager.FindByIdAsync(command.RoleId);
+
+			return role != null;
+		}
+
+		private async Task<bool> ValidateNotChangingSuperAdmin(SetRolePermissionsCommand command)
+		{
+			var role = await _roleManager.FindByIdAsync(command.RoleId);
+
+			return role.Name != RoleStrings.SuperAdmin;
+		}
 	}
 }
